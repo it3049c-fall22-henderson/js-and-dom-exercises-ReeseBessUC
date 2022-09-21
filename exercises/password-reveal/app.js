c0cc279a2d80a4bd60e55a69e875abdaaffa74a0
ReeseBessUC <112289216+ReeseBessUC@users.noreply.github.com>
2022-09-21 13:36:06 -0400

    Update app.js

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,7 +1,13 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
 
-
 // 🤖: Create an Event Listener on the password reveal button
+<div class="password">
+    <form>
+            <label for="pswd">Enter password: </label>
+            <input type="password" id="pswd"/>
+            <input type="button" id="myBtn" value="submit" onclick="checkPswd();" />
+        </form>
+    </div>
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
 
 
